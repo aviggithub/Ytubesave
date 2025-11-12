42fa587b195e1b196b66b4e10d6d7dc921cef947
Avinash G <46967951+aviggithub@users.noreply.github.com>
2025-11-12 18:04:31 +0530

    Update ytube_video_save.py

--- Ytube Video Downloader/ytube_video_save.py
+++ Ytube Video Downloader/ytube_video_save.py
@@ -1,5 +1,6 @@
 from pytubefix import YouTube
 from pytubefix.cli import on_progress  # optional for progress
-yt = YouTube('https://www.youtube.com/watch?v=A0nnD9abDBAhfhfhfhfhfgh', on_progress_callback=on_progress)
+yt = YouTube('🎥 Enter YouTube URL: ', on_progress_callback=on_progress)
 yt.streams.get_highest_resolution().download()
+
 print("Download Complete!")
